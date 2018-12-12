fffaeef3ccdcc1196782b20b1c78ed8c7d27a814
Ogutu Brian <codingbrian58@gmail.com>
2018-12-12 14:18:20 +0300

    [Finishes #162554289] added view question page with an answer button

--- UI/js/button_events.js
+++ UI/js/button_events.js
@@ -2,3 +2,7 @@ function deleteEvent(quest_id,btn_id) {
     document.getElementById(quest_id).innerHTML = "";
     document.getElementById(btn_id).innerHTML="";
 }
+
+function logInbtnBtnHandler(){
+    window.location.href ="questions.html";
+}
