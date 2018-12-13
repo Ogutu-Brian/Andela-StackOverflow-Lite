644f73957f57061af7b44fad0749a58a241ccc2f
Ogutu Brian <codingbrian58@gmail.com>
2018-12-13 12:32:58 +0300

    [Delivers #162554795] formats the code

--- UI/js/button_events.js
+++ UI/js/button_events.js
@@ -1,11 +1,11 @@
-function deleteEvent(quest_id,btn_id) {
+function deleteEvent(quest_id, btn_id) {
     document.getElementById(quest_id).innerHTML = "";
-    document.getElementById(btn_id).innerHTML="";
+    document.getElementById(btn_id).innerHTML = "";
 }
 
-function logInbtnBtnHandler(){
-    window.location.href ="questions.html";
+function logInbtnBtnHandler() {
+    window.location.href = "questions.html";
 }
-function makeFavourite(ans_id){
+function makeFavourite(ans_id) {
     document.getElementById(ans_id).className += " fa fa-star";
 }
